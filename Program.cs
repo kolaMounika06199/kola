40d6d03fd06b7c5894c3745a837641068073a781
kolaMounika06199 <mounikakondagari06@gmail.com>
2020-10-30 16:24:53 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @delegate
+{
+    class Program
+    {
+       
+        public int Add(int a, int b)
+        {
+            Console.WriteLine(a * b);
+            return a * b;
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Func<int,int> f = p.Add;
+            int res = f(4, 6);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
